6ae43d21af0da8303b13867c9e44c1c50e4ecaa2
Jovany <43921445+jovanyvelez@users.noreply.github.com>
2026-05-06 23:15:23 -0500

    cambios

--- README.md
+++
@@ -1,2 +0,0 @@
-# web_sin_herencia
-Usa plantillas pero sin ningún tipo de herencia
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+from datetime import datetime
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from datetime import datetime
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 @app.get("/", response_class=HTMLResponse)
